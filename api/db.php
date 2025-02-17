3a5c1033a93e0c0370471768c6f7950b66783c0f
reg <reg8740223@gmail.com>
2025-02-17 16:35:46 +0800

    modified:   api/db.php 修正count的判斷字串問題 	modified:   back/edit_item.php 修正返回按鈕功能 	modified:   front/detail.php 加上分類文字 	modified:   index.php 修正商品分類數量顯示

--- api/db.php
+++ api/db.php
@@ -79,14 +79,14 @@ function count(...$arg){
     if(!empty($arg[0]) && is_array($arg[0])){
         $tmp=$this->arrayToSQL($arg[0]);
         $sql .=" where ".join(" && ",$tmp);
-    }else if(is_string($arg[0])){
+    }else if(isset($arg[0]) && is_string($arg[0])){
         $sql .=$arg[0];
     }
 
 if(!empty($arg[1])){
     $sql .= $arg[1];
 }
-
+    //echo $sql;
     return $this->pdo->query($sql)->fetchColumn();
 }
 
